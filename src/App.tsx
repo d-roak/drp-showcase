8c6944f1dd3ef0b72e5edb3266b9fd837d120467
droak <me+git@droak.sh>
2025-01-13 04:06:37 +0530

    fix: properly render peers

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import { DRPNode } from "@ts-drp/node";
 import { formatPeerId } from "./utils/formatting";
+import { useEffect, useState } from "react";
 
 function App(props: { node: DRPNode }) {
+	const [peers, setPeers] = useState<string[]>([]);
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setPeers(props.node.networkNode.getAllPeers());
+		}, 1000);
+		return () => clearInterval(interval);
+	}, [props.node.networkNode]);
+
 	return (
 		<div className="w-screen text-center">
 			<h1>DRP Showcase</h1>
@@ -9,7 +18,7 @@ function App(props: { node: DRPNode }) {
 				<p>Your peer id is: {formatPeerId(props.node.networkNode.peerId)}</p>
 				<p>
 					Your connections:{" "}
-					{props.node.networkNode.getAllPeers().map((peer, index) => (
+					{peers.map((peer, index) => (
 						<span key={index}>
 							{formatPeerId(peer)}
 							{index < props.node.networkNode.getAllPeers().length - 1 && ", "}
