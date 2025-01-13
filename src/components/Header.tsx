bc8995d1a471b11d67a0c99fe144ecb7a0174129
winprn <minhladz001@gmail.com>
2025-01-14 02:33:05 +0700

    feat: show more info and handle more actions

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,29 +3,107 @@ import { formatPeerId } from "../utils/formatting";
 import type { DRPNode } from "@ts-drp/node";
 
 export default function Header(props: { node: DRPNode }) {
-	const [peers, setPeers] = useState<string[]>([]);
-	useEffect(() => {
-		const interval = setInterval(() => {
-			setPeers(props.node.networkNode.getAllPeers());
-		}, 1000);
-		return () => clearInterval(interval);
-	}, [props.node.networkNode]);
+    const [peers, setPeers] = useState<string[]>([]);
+    const [peerId, setPeerId] = useState<string>("");
+    const [bootstraps, setBootstraps] = useState<string[]>(props.node.networkNode.getBootstraps());
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setPeers(props.node.networkNode.getAllPeers());
+        }, 1000);
+        return () => clearInterval(interval);
+    }, [props.node.networkNode]);
 
-	return (
-		<header className="mx-auto w-fit mt-10">
-			<h1>DRP Showcase</h1>
-			<div className="mb-10">
-				<p>Your peer id is: {formatPeerId(props.node.networkNode.peerId)}</p>
-				<p>
-					Your connections:{" "}
-					{peers.map((peer, index) => (
-						<span key={index}>
-							{formatPeerId(peer)}
-							{index < props.node.networkNode.getAllPeers().length - 1 && ", "}
-						</span>
-					))}
-				</p>
-			</div>
-		</header>
-	);
+    const removeBootstrap = (node: string) => {
+        try {
+            const newBootstraps = bootstraps.filter((n) => n !== node);
+            props.node.networkNode.disconnect(node.split("/").pop()!);
+            setBootstraps(newBootstraps);
+            props.node.networkNode.restart({
+                bootstrap_peers: newBootstraps,
+            });
+            console.log("newBootstraps", newBootstraps);
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    const handleConnect = async () => {
+        if (!peerId) {
+            return;
+        }
+        await props.node.networkNode.connect(peerId);
+    };
+
+    return (
+        <header className="mx-auto w-fit mt-10">
+            <h1>DRP Showcase</h1>
+            <div className="mb-10">
+                <div className="flex gap-2 items-center">
+                    <p>Your peer id is: {formatPeerId(props.node.networkNode.peerId)}</p>
+                    <button
+                        className="py-1"
+                        type="button"
+                        onClick={() => {
+                            navigator.clipboard.writeText(props.node.networkNode.peerId);
+                        }}
+                    >
+                        Copy
+                    </button>
+                </div>
+                <div className="flex gap-2 items-center">
+                    <p>Your Multiaddr: {props.node.networkNode.getMultiaddrs() ? props.node.networkNode.getMultiaddrs()[0] : ""}</p>
+                    <button
+                        className="py-1"
+                        type="button"
+                        onClick={() => {
+                            navigator.clipboard.writeText(props.node.networkNode.getMultiaddrs()[0]);
+                        }}
+                    >
+                        Copy
+                    </button>
+                </div>
+                <p>
+                    Your bootstraps:{" "}
+                    {bootstraps.map((node, index) => (
+                        <span key={node}>
+                            {formatPeerId(node.split("/").pop() ? node.split("/").pop()! : "")}
+                            <button
+                                className="px-2 py-1 bg-red-700 ml-2"
+                                onClick={() => {
+                                    removeBootstrap(node);
+                                }}
+                                type="button"
+                            >
+                                Del
+                            </button>
+                            {index < bootstraps.length - 1 && ", "}
+                        </span>
+                    ))}
+                </p>
+                <p>
+                    Your connections:{" "}
+                    {peers.map((peer, index) => (
+                        <span key={index}>
+                            {formatPeerId(peer)}
+                            {index < props.node.networkNode.getAllPeers().length - 1 && ", "}
+                        </span>
+                    ))}
+                </p>
+                <div className="flex gap-2 items-center">
+                    <p>Connect to new peer:</p>
+                    <input
+                        className="border-1 border-gray-500 rounded-md p-1"
+                        placeholder="Peer ID"
+                        value={peerId}
+                        onChange={(e) => {
+                            setPeerId(e.target.value);
+                        }}
+                    />
+                    <button className="py-1" type="button" onClick={handleConnect}>
+                        Connect
+                    </button>
+                </div>
+            </div>
+        </header>
+    );
 }
