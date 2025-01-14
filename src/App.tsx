16debdb75db5c09a0f43c6dc2a005e9e0495dcbb
droak <me+git@droak.sh>
2025-01-14 07:17:13 +0530

    fix: rm unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
 import { DRPNode } from "@ts-drp/node";
 import Header from "./components/Header";
-import {
-	BrowserRouter,
-	createBrowserRouter,
-	Route,
-	RouterProvider,
-	Routes,
-} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Grid from "./components/Grid";
 import Home from "./components/Home";
 
